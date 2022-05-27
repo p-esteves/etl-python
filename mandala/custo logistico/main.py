5c00583b7a45d670e6dcb148b11703297f48842b
p-esteves <70980614+p-esteves@users.noreply.github.com>
2022-05-27 12:25:12 -0300

    -

--- mandala/custo logistico/main.py
+++ mandala/custo logistico/main.py
@@ -6,17 +6,19 @@ import html5lib
 import requests
 from urllib.request import urlopen
 from bs4 import BeautifulSoup
+import lxml.html as lh
 
-url = "https://www.3plogisticSs.com/3pl-market-info-resources/3pl-market-information/global-3pl-market-size-estimates/"
+#url = "https://www.3plogistics.com/3pl-market-info-resources/3pl-market-information/global-3pl-market-size-estimates/"
 
+#df.columns = ["NM_VARIAVEL","NM_PAIS","ANO","NM_VALOR_INDICADOR"]
 
+#df.reset_index(drop=True,inplace=True)
 
+df = pd.read_excel("dados.xlsx")
 
-df.columns = ["NM_VARIAVEL","NM_PAIS","ANO","NM_VALOR_INDICADOR"]
-
-df.reset_index(drop=True,inplace=True)
+df.drop(df.iloc[:, 6:10], inplace=True, axis=1)
 
 con = db_connect(package = 'sqlalchemy')
 cols = list(df.columns)
-df.to_sql('', con, if_exists = 'replace', index = False, chunksize = int(2100/(len(cols) + 1)), method = 'multi')
+df.to_sql('CUSTO_LOGISTICO', con, if_exists = 'replace', index = False, chunksize = int(2100/(len(cols) + 1)), method = 'multi')
 con.close()
